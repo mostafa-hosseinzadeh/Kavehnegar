fa7d1eba17f74f6a13842d28319bc88ac429aaef
mostafa.hoseinzade <mostafa.hoseinzade@NO242-PC>
2020-08-16 11:35:12 +0430

    Add project files.

---
+++ TestDotNetPakages/Models/AccountInfoResult.cs
@@ -0,0 +1,15 @@
+using System;
+using Kavenegar.Utils;
+namespace Models
+{
+ public class AccountInfoResult
+ {
+	public long RemainCredit { get; set; }
+	public long Expiredate { get; set; }
+	public DateTime GregorianExpiredate
+	{
+	 get { return  DateHelper.UnixTimestampToDateTime(Expiredate); }
+	}
+	public string Type { get; set; }
+ }
+}
